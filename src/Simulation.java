ed2a3743926f99b1ab08055ab8f16ff65183d20c
Manuel Valdez <manuel.valdez@fundacion-jala.org>
2019-02-17 12:05:44 -0400

    Project structure added.

---
+++ src/Simulation.java
@@ -0,0 +1,24 @@
+import rockets.Rocket;
+import rockets.U1;
+import rockets.U2;
+
+import java.util.ArrayList;
+import items.Item;
+
+public class Simulation  {
+    public ArrayList<Item> loadItems(String fileName) {
+        return new ArrayList<Item>();
+    }
+
+    public ArrayList<U1> loadU1(ArrayList<String> itemsToLoad) {
+        return new ArrayList<U1>();
+    }
+
+    public ArrayList<U2> loadU2(ArrayList<String> itemsToLoad) {
+        return new ArrayList<U2>();
+    }
+
+    public double runSimulation(ArrayList<Rocket> rockets) {
+        return 0.0;
+    }
+}
