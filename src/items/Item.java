39366110e35c06be7aa162d3fab076f970589f8a
JimmyRomero <jimmy.romero@fundacion-jala.org>
2019-02-17 22:45:47 -0400

    Item and rocket classes and methods added

--- src/items/Item.java
+++ src/items/Item.java
@@ -3,4 +3,17 @@ package items;
 public class Item {
     private String name;
     private int weight;
+
+    public Item(String name, int weight){
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public  String getName(){
+        return name;
+    }
+
+    public int getWeight(){
+        return weight;
+    }
 }
--- src/rockets/Rocket.java
+++ src/rockets/Rocket.java
@@ -3,18 +3,65 @@ package rockets;
 import items.Item;
 
 public class Rocket implements SpaceShip {
+    private double currentWeight;
+    private double maxCargo;
+    private double weigth;
+    private double cost;
+
+    @Override
     public boolean launch() {
         return true;
     }
+
+    @Override
     public boolean land() {
         return true;
     }
-    public final boolean canCarry(Item itemToCarry) {
-        // TODO implement this method
-        return true;
-    }
-    public final void carry(Item itemToCarry) {
-        // TODO implement this method
 
+    @Override
+    public final boolean canCarry(Item itemToCarry) {
+        if (maxCargo >= currentWeight + itemToCarry.getWeight())
+            return true;
+        else
+            System.out.println("Can't carry item: " + itemToCarry.getName() + "\tWeigth: " + itemToCarry.getWeight());
+        return false;
+    }
+
+    @Override
+    public final void carry(Item itemToCarry) {
+        System.out.println("Carry item: " + itemToCarry.getName() + "\tWeight: " + itemToCarry.getWeight());
+        currentWeight += itemToCarry.getWeight();
+    }
+
+    public void setMaxCargo(double maxCargo) {
+        this.maxCargo = maxCargo;
+    }
+
+    public double getmaxCargo() {
+        return maxCargo;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public void setWeigth(double weight) {
+        this.weigth = weight;
+    }
+
+    public double getWeigth() {
+        return weigth;
+    }
+
+    public void setCurrentWeight(double currentWeight) {
+        this.currentWeight = currentWeight;
+    }
+
+    public double getCurrentWeight() {
+        return currentWeight;
+    }
+
+    public double getTotalWeight() {
+        return getCurrentWeight() + getWeigth();
     }
 }
